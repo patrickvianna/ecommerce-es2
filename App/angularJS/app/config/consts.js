89981ba26906c4be06d92c1dfc36f0cc4cf6d805
Patrick Vianna <patrickviannapblv@gmail.com>
2018-05-07 09:27:37 -0300

    Variaveis produção API

--- App/angularJS/app/config/consts.js
+++ App/angularJS/app/config/consts.js
@@ -4,8 +4,8 @@ angular.module('myApp').constant('consts', {
     owner: 'Patrick',
     year: '2018',
     site: '',
-    apiUrl: 'http://localhost:3003/api',
-    oapiUrl: 'http://localhost:3003/oapi',
+    apiUrl: 'https://ecommerce-es2.appspot.com/api',
+    oapiUrl: 'https://ecommerce-es2.appspot.com/oapi',
     userKey: '_my_app_user'
 }).run(['$rootScope', 'consts', function ($rootScope, consts) {
     $rootScope.consts = consts
